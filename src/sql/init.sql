606ab4959608703c00e4065662091555e4ca92f6
Marcus Gruneau <marcus.gruneau@gmail.com>
2018-12-01 18:00:18 +0100

    support for querying questions 1-6 added

--- src/sql/init.sql
+++ src/sql/init.sql
@@ -59,6 +59,27 @@ INSERT INTO guide (social_sec_no, name) VALUES ('123456-7890', 'John Doe');
 INSERT INTO guide (social_sec_no, name) VALUES ('234567-8901', 'Tim Cook');
 INSERT INTO guide (social_sec_no, name) VALUES ('345678-9012', 'Fred Foo');
 
+CREATE TABLE guide_language (
+	social_sec_no VARCHAR(255),
+	language VARCHAR(255),
+	PRIMARY KEY(social_sec_no, language),
+	FOREIGN KEY fk_gl_guide(social_sec_no) REFERENCES guide(social_sec_no)
+	ON UPDATE CASCADE
+	ON DELETE RESTRICT,
+	FOREIGN KEY fk_gl_lang(language) REFERENCES language(name)
+	ON UPDATE CASCADE
+	ON DELETE RESTRICT
+);
+
+INSERT INTO guide_language (social_sec_no, language) VALUES
+('123456-7890', 'English'),
+('123456-7890', 'German'),
+('345678-9012', 'German'),
+('234567-8901', 'English'),
+('234567-8901', 'French'),
+('345678-9012', 'Russian');
+
+
 CREATE TABLE artist (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(255) NOT NULL,
@@ -94,6 +115,7 @@ CREATE TABLE artwork (
 INSERT INTO artwork (artist_id, name, description, size, created_at, artwork_type)
 VALUES
 ((SELECT id FROM artist WHERE name = 'Salvador Dalí'), 'The Persistence of Memory', 'Four clocks are prominently on display in an otherwise empty desert scene. While this might seem uncanny enough, the clocks are not flat as you might expect them to be, but are bent out of shape, appearing to be in the act of melting away.', 4, '1931-1-1', 'Painting'),
+((SELECT id FROM artist WHERE name = 'Salvador Dalí'), 'The Temptation of St. Anthony', 'In this picture temptation appears to Saint Anthony successively in the form of a horse in the foreground representing strength, sometimes also the symbol of voluptuousness.', 4, '1946-1-1', 'Painting'),
 ((SELECT id FROM artist WHERE name = 'Pablo Picasso'), 'Guernica', 'The painting was created in response to the bombing of Guernica, a Basque Country town in northern Spain, by Nazi Germany and Italian warplanes at the request of the Spanish Nationalists.', 8, '1937-1-1', 'Painting'),
 ((SELECT id FROM artist WHERE name = 'Frida Kahlo de Rivera'), 'The Two Fridas', 'In Frida\'s dairy, she wrote about this painting and said it is originated from her memory of an imaginary childhood friend. Later she admitted it expressed her desperation and loneliness with the separation from Diego.', 2, '1939-1-1', 'Painting');
 
@@ -132,6 +154,86 @@ INSERT INTO product_artwork (artwork_id, product_barcode)
 VALUES (1,'123456789');
 
 INSERT INTO product (barcode, price, type, size, color)
-VALUES ('555556722', 5000, 'Postcard', NULL, NULL);
+VALUES ('992245762', 5000, 'Postcard', NULL, NULL);
+INSERT INTO product_artwork (artwork_id, product_barcode)
+VALUES (1,'992245762');
+
+INSERT INTO product (barcode, price, type, size, color)
+VALUES ('555556722', 5000, 'T-shirt', NULL, NULL);
 INSERT INTO product_artwork (artwork_id, product_barcode)
 VALUES (2,'555556722');
+
+
+CREATE TABLE exhibition (
+	id 	INT PRIMARY KEY AUTO_INCREMENT,
+	name  VARCHAR(255) NOT NULL,
+	start DATE NOT NULL,
+	end DATE NOT NULL,
+	size INT NOT NULL
+);
+
+CREATE TABLE artwork_exhibition (
+   artwork_id INT,
+   exhibition_id INT,
+   PRIMARY KEY(artwork_id, exhibition_id),
+   FOREIGN KEY ae_artwork(artwork_id) REFERENCES artwork(id)
+   ON UPDATE CASCADE
+   ON DELETE RESTRICT,
+   FOREIGN KEY ae_exhibition(exhibition_id) REFERENCES exhibition(id)
+   ON UPDATE CASCADE
+   ON DELETE RESTRICT
+);
+
+INSERT INTO exhibition (name, start, end, size) VALUES
+('The grand opening', '2018-11-30', '2018-11-30', 150),
+('New year', '2018-12-31', '2018-12-31', 200);
+
+INSERT INTO artwork_exhibition (artwork_id, exhibition_id) VALUES
+(1,1),
+(2,1),
+(2,2),
+(3,2),
+(4,2);
+
+
+CREATE TABLE exhibition_guide (
+	social_sec_no VARCHAR(255),
+	exhibition_id INT,
+	PRIMARY KEY (social_sec_no, exhibition_id),
+	FOREIGN KEY eg_guide(social_sec_no) REFERENCES guide(social_sec_no)
+	ON UPDATE CASCADE
+	ON DELETE RESTRICT,
+	FOREIGN KEY eg_exhibition(exhibition_id) REFERENCES exhibition(id)
+	ON UPDATE CASCADE
+	ON DELETE RESTRICT	
+);
+
+INSERT INTO exhibition_guide (social_sec_no, exhibition_id) VALUES
+('123456-7890', 1),
+('123456-7890', 2),
+('234567-8901', 2);
+
+CREATE TABLE guided_tour (
+	exhibition_id INT NOT NULL,
+	guide_social_sec_no VARCHAR(255) NOT NULL,
+    language VARCHAR(255) NOT NULL,
+    start TIMESTAMP NOT NULL,
+    end TIMESTAMP NOT NULL,
+    PRIMARY KEY (guide_social_sec_no, start),
+    FOREIGN KEY gt_exhibition(exhibition_id) REFERENCES exhibition(id)
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT,
+    FOREIGN KEY gt_guide(guide_social_sec_no) REFERENCES guide(social_sec_no)
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT,
+    FOREIGN KEY gt_language(language) REFERENCES language(name)
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT
+);
+
+INSERT INTO guided_tour (exhibition_id, guide_social_sec_no, language, start, end) VALUES
+(1, '123456-7890', 'English', '2018-11-30 10:00:00', '2018-11-30 11:00:00'),
+(1, '123456-7890', 'German', '2018-11-30 11:00:00', '2018-11-30 12:00:00'),
+(2, '123456-7890', 'English', '2018-12-31 13:00:00', '2018-12-31 14:00:00'),
+(2, '234567-8901', 'French', '2018-12-31 11:00:00', '2018-12-31 12:00:00');
+
